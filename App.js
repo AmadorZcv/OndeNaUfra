513ee16f9672d50ad8c7ffec30f445d90995f2d8
leoayresec <leoayresec@gmail.com>
2018-07-16 17:36:48 -0300

    Recuperando a localização no Mapa.

--- App.js
+++ App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from "react-native";
+import { MapView } from 'expo';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      region: {
+        latitude: 0.0000,
+        longitude: 0.0000,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    };
+  };
+  positionSucess(position) {
+    const regionNova = {
+      latitude: position.coords.latitude, 
+      longitude: position.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }
+    console.log("Aqui position ",position);
+    this.setState({ region:regionNova});
+  }
+  positionError(error) {
+    Alert.alert("Erro",error);
+
+  }
+  componentDidMount() {
+    this.getPosition()
+  }
+  getPosition() {
+    this.setState({ isFetchingPosition: true });
+    navigator.geolocation.getCurrentPosition(
+      position => this.positionSucess(position),
+      error => this.positionError(error),
+      { enableHighAccuracy: false, timeout: 10000, maximumAge: 10000 }
+    );
+
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <MapView
+        style={{ flex: 1 }}
+        region={this.state.region}
+        /*initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}*/
+      />
     );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+
+
+}
