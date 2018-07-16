e928f53d2b5f8cc165e22f578519504f75e461e8
Amador <lucasamadorzcv@gmail.com>
2018-07-16 18:30:07 -0300

    Posição do usuario

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Alert } from "react-native";
-import { MapView } from 'expo';
+import {Alert} from 'react-native';
+import {MapView} from 'expo';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor (props) {
+    super (props);
 
     this.state = {
       region: {
@@ -12,50 +12,53 @@ export default class App extends React.Component {
         longitude: 0.0000,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
-      }
+      },
     };
-  };
-  positionSucess(position) {
+  }
+  positionSucess (position) {
     const regionNova = {
-      latitude: position.coords.latitude, 
+      latitude: position.coords.latitude,
       longitude: position.coords.longitude,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
-    }
-    console.log("Aqui position ",position);
-    this.setState({ region:regionNova});
+    };
+    console.log ('Aqui position ', position);
+    this.setState ({region: regionNova});
   }
-  positionError(error) {
-    Alert.alert("Erro",error);
-
+  positionError (error) {
+    Alert.alert ('Erro', error);
   }
-  componentDidMount() {
-    this.getPosition()
+  componentDidMount () {
+    this.getPosition ();
   }
-  getPosition() {
-    this.setState({ isFetchingPosition: true });
-    navigator.geolocation.getCurrentPosition(
-      position => this.positionSucess(position),
-      error => this.positionError(error),
-      { enableHighAccuracy: false, timeout: 10000, maximumAge: 10000 }
+  getPosition () {
+    this.setState ({isFetchingPosition: true});
+    navigator.geolocation.getCurrentPosition (
+      position => this.positionSucess (position),
+      error => this.positionError (error),
+      {enableHighAccuracy: false, timeout: 10000, maximumAge: 10000}
     );
-
   }
-  render() {
+  render () {
     return (
       <MapView
-        style={{ flex: 1 }}
+        style={{flex: 1}}
         region={this.state.region}
+        showsUserLocation={true}
         /*initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}*/
-      />
+      >
+        <MapView.Marker
+          coordinate={{
+            latitude: this.state.region.latitude,
+            longitude: this.state.region.longitude,
+          }}
+        />
+      </MapView>
     );
-
   }
-
-
 }
