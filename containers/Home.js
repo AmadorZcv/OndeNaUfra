938d385dc16c5dc1806e42d3ce39b75264d88d70
Amador <lucasamadorzcv@gmail.com>
2018-08-13 02:10:45 -0300

    Remoção de metodos não mais utilizados

--- containers/Home.js
+++ containers/Home.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import {Alert} from 'react-native';
 import {MapView} from 'expo';
-//import {database} from '../config/firebase';
 import {listenLugares} from '../config/database';
 export default class Home extends Component {
   //Iniciar os estados necessarios para funcionar
   constructor (props) {
     super (props);
-
     this.state = {
-      region: {
+      //Dados da UFRA Portão Principal
+      initialRegion: {
         latitude: -1.454827,
         longitude: -48.445922,
         latitudeDelta: 0.0922,
@@ -21,30 +20,6 @@ export default class Home extends Component {
     };
   }
   //Em caso de sucesso no fetch da position
-  positionSucess (position) {
-    const regionNova = {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    };
-    //Criamos um objeto região novo e salvamos no estado
-    this.setState ({region: regionNova});
-  }
-  positionError (error) {
-    const regionNova = {
-      latitude: -1.454827,
-      longitude: -48.445922,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    };
-    //Em caso de erro criamos uma região nova focada no predio central
-    this.setState ({region: regionNova});
-    //Log para debug,retirar em modo de produção
-    //console.log ('Erro foi ', error);
-    //Erro para o usuario
-    Alert.alert ('Erro', 'Não foi possivel achar sua posição');
-  }
   getLugares (snapshot) {
     const valor = snapshot.val ();
     const oldMarkers = this.state.markers;
@@ -59,8 +34,6 @@ export default class Home extends Component {
   }
   //Quando o componente montar
   componentDidMount () {
-    //Pegamos a posição
-    this.getPosition ();
     //Criamos um listener que executa função sempre que adicionamos uma nova posição
     //Devido ao funcionamento do firebase só precisamos chamar isso para carregar os pontos iniciais tambem
     listenLugares (snapshot => this.getLugares (snapshot));
@@ -73,14 +46,6 @@ export default class Home extends Component {
     const {local} = localSnap.val ();
     trueMarkers.push ({local: local, key: localSnap.key});
     this.setState ({markers: trueMarkers});
-  }
-  getPosition () {
-    this.setState ({isFetchingPosition: true});
-    navigator.geolocation.getCurrentPosition (
-      position => this.positionSucess (position),
-      error => this.positionError (error),
-      {enableHighAccuracy: false, timeout: 10000, maximumAge: 10000}
-    );
   }
   //Função que carrega os marcadores no mapa
   renderMarkers () {
@@ -99,7 +64,7 @@ export default class Home extends Component {
     return (
       <MapView
         style={{flex: 1, marginTop: this.state.marginTop}}
-        initialRegion={this.state.region}
+        initialRegion={this.state.initialRegion}
         showsUserLocation={true}
         showsMyLocationButton={true}
       >
